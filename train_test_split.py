74d1e897627d2c02cc56521a0988e198bcc45bd4
elenagiraldo3 <elenagiraldo3@gmail.com>
2022-10-22 12:36:43 +0200

    train_test_split.py pasar dataset por argumento

--- train_test_split.py
+++ train_test_split.py
@@ -1,15 +1,21 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
-from PIL import Image
-from skimage import io
+import argparse
+import os
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', help='Path to dataset')
+parser.add_argument('--testSize', type=float, default=0.20, help='Size of test dataset')
+parser.add_argument('--validationSize', type=float, default=0.20, help='Size of validation dataset')
+
+args = parser.parse_args()
 # split the data into train, validation and test set
-data = pd.read_csv("C:/Users/elena/Documents/TFM/AA_TAG_IMAGENES/datosContenedorGO.csv",
-                   delimiter=';', encoding='ISO-8859-1')
+folder = os.path.dirname(args.dataset)
+data = pd.read_csv(args.dataset, delimiter=';', encoding='ISO-8859-1')
 # full, _ = train_test_split(data, test_size=0.40, random_state=0)
-train, test = train_test_split(data, test_size=0.20, random_state=0)
-train, validation = train_test_split(train, test_size=0.20, random_state=0)
+train, test = train_test_split(data, test_size=args.testSize, random_state=0)
+train, validation = train_test_split(train, test_size=args.validationSize, random_state=0)
 # save the data
-train.to_csv('C:/Users/elena/Documents/TFM/AA_TAG_IMAGENES/train.csv', index=False)
-validation.to_csv('C:/Users/elena/Documents/TFM/AA_TAG_IMAGENES/validation.csv', index=False)
-test.to_csv('C:/Users/elena/Documents/TFM/AA_TAG_IMAGENES/test.csv', index=False)
+train.to_csv(f'{folder}/train.csv', index=False)
+validation.to_csv(f'{folder}/validation.csv', index=False)
+test.to_csv(f'{folder}/test.csv', index=False)
